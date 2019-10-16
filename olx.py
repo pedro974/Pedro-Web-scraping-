4687125546a46b35f1ff3f601ed0c977d105aaed
pedro974 <54594354+pedro974@users.noreply.github.com>
2019-10-16 17:49:59 -0300

    Add files via upload

--- olx.py
+++ olx.py
@@ -1,6 +1,7 @@
 from selenium import  webdriver
-import csv
-#f = open('olx.csv','w')
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
 class google:
     def __init__(self,driver):
         self.driver = driver
@@ -8,33 +9,58 @@ class google:
         self.search_b = 'q'
         self.pesquisar_boton = 'submitBtn'              
         self.titulo= 'OLXad-list-title' #class titulo
-        self.local = 'text detail-region' #classe local
+        self.local = 'OLXad-list-line-2' #classe local
         self.categoria = 'text detail-category' #class categoria
-        self.preco = 'OLXad-list-price' #class preco
+        self.preco = 'col-3' #class preco
         self.horario = 'col-4' #class horario
+        self.next_pagina = 'item next'
     def navigate(self):
          self.driver.get(self.url)
     def search(self, word = 'None'):
         self.driver.find_element_by_name(self.search_b).send_keys(word)
         self.driver.find_element_by_class_name(self.pesquisar_boton).click()
     def printx(self):
+        h= 1
         a =self.driver.find_elements_by_class_name(self.titulo)
         for g in a:
-                t =()
-                gx = g.text
-                f =[gx]
-                t = f
-                print(t)
+            h +=1
+            gx = g.text
+            ws.cell(row=h,column =2, value = gx)
+            print(h)
         b =self.driver.find_elements_by_class_name(self.preco)
+        hx = 1
+        for g in b:
+            hx +=1
+            gx = g.text
+            ws.cell(row=hx,column =3, value = gx)
+            print(hx)
         c =self.driver.find_elements_by_class_name(self.local)
+        hg = 1
+        for g in c:
+            hg +=1
+            gx = g.text
+            ws.cell(row=hg,column =4, value = gx)
+            print(hg)
+        hy = 1
         d =self.driver.find_elements_by_class_name(self.horario)
+        for g in d:
+            hy +=1
+            gx = g.text
+            ws.cell(row=hy,column =5, value = gx)
+            print(hy)
         e =self.driver.find_elements_by_class_name(self.categoria)
-        h = [a,b,c,d,e]
+        hh = 1
+        for g in e:
+            hh +=1
+            gx = g.text
+            ws.cell(row=hh,column =6, value = gx)
+            print(hh)
+        def pagina(self):
+            self.driver.find_element_by_class_name(self.next_pagina).click()
        
         
 
-         
-
+        
         
         
                     
@@ -44,11 +70,13 @@ gg = webdriver.Chrome() #abre o driver
 g = google(gg) #abre o google
 g.navigate() #abre navegador
 g.search('Celular') #c
+g.printx
+wb.save('olx.xlsx')
+g.pagina()
 g.printx()
+wb.save('olx.xlsx')
 gg.close()
-#f = csv.write(f)
-#write.writerow(gx)
-f.close()
+
 
 """for local in c:
 #print(local.text)
